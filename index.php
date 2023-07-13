f24aba2c628c699e4ae7eaae98d32536ce1b4ed0
Yoriqulov Oybek <hhoybek@gmail.com>
2023-07-13 17:37:16 +0500

    arrays

--- index.php
+++ index.php
@@ -1,8 +1,23 @@
 <?php
+$numbers = [1, 2, 3];
+$fruits = array('apple', 'banana', 'kiwi');
 
-$name='John Doe';
-$age=24;
+// Associative array
+$person = [
+    'first_name' => 'John',
+    'last_name' => 'Doe',
+    'age' => 24,
+    'hobby' => 'sport'
+];
 
-echo "<h1>{$name} is {$age} years old.</h1>"
+//echo "<h1 style='color: red'>$person[first_name] $person[last_name]</h1> is $person[age] years old and his hobby is $person[hobby]."
+
+// Multidimensional arrays
+$people = [
+    ['name' => 'Jane', 'age' => 34]
+];
+
+echo $people[0]['name'];
+var_dump(json_encode($people));
 
 ?>
