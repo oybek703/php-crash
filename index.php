ceefbb35b743e5f48fd46958d2f359cb2e51a2e1
Yoriqulov Oybek <hhoybek@gmail.com>
2023-07-13 23:11:05 +0500

    file operations

--- index.php
+++ index.php
@@ -1,26 +1,16 @@
 <?php
-session_start();
-if (isset($_POST['submit'])) {
-    $username= filter_input(INPUT_POST, 'username', FILTER_SANITIZE_SPECIAL_CHARS);
-    $password= filter_input(INPUT_POST, 'password', FILTER_SANITIZE_SPECIAL_CHARS);
-    if ($username === 'John' && $password === '123456') {
-        $_SESSION['username'] = $username;
-        header('Location: /php-crash/dashboard.php');
-    } else{
-        echo  '<h4 style="color: red">Invalid credentials!</h4>';
-    }
+$filename = 'users2.txt';
+
+if (file_exists($filename)) {
+    $handle = fopen($filename, 'r');
+    $contents = fread($handle, filesize($filename));
+    fclose($handle);
+    echo $contents;
+} else {
+    $handle = fopen($filename, 'w');
+    $contents = "Brad \nTime \nJane \nJohn";
+    fwrite($handle, $contents);
+    fclose($handle);
 }
 
 ?>
-
-<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="post">
-    <div>
-        <label for="username">Username: </label>
-        <input type="text" name="username">
-    </div>
-    <div>
-        <label for="password">Password:</label>
-        <input type="password" name="password">
-    </div>
-    <input type="submit" value="Submit" name="submit">
-</form>
--- logout.php
+++
@@ -1,4 +0,0 @@
-<?php
-session_destroy();
-session_destroy();
-header('Location: /php-crash');
