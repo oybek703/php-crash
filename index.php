60637ecd7cba844ea31a642dddb6faf47b4f250c
Yoriqulov Oybek <hhoybek@gmail.com>
2023-07-13 23:37:50 +0500

    file upload start

--- index.php
+++ index.php
@@ -1,16 +1,29 @@
 <?php
-$filename = 'users2.txt';
-
-if (file_exists($filename)) {
-    $handle = fopen($filename, 'r');
-    $contents = fread($handle, filesize($filename));
-    fclose($handle);
-    echo $contents;
-} else {
-    $handle = fopen($filename, 'w');
-    $contents = "Brad \nTime \nJane \nJohn";
-    fwrite($handle, $contents);
-    fclose($handle);
+if (isset($_POST['submit'])) {
+    if (!empty($_FILES['upload']['name'])) {
+        print_r($_FILES);
+        $file_name = $_FILES['upload']['name'];
+    } else {
+        echo "<h3 style='color: red'>Plase choose file!</h3>";
+    }
 }
-
 ?>
+
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post" enctype="multipart/form-data">
+    Select file to upload:
+    <br>
+    <input type="file" name="upload">
+    <br>
+    <input name="submit" type="submit" value="Submit">
+</form>
+</body>
+</html>
