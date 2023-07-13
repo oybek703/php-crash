ab1da70d42ac5d095dad6843c8bf6b4277cd42ee
Yoriqulov Oybek <hhoybek@gmail.com>
2023-07-13 22:23:42 +0500

    forms

--- index.php
+++ index.php
@@ -15,18 +15,24 @@ function get_query_params(array $params_array): string
 
 $get_link = fn() => "<a href='{${get_query_params($_GET)}}'>Click</a>";
 
+if (isset($_POST['submit'])) {
+    echo $_POST['name'];
+    echo $_POST['age'];
+}
+
+echo $get_link();
+
 ?>
 
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-<?php echo $get_link() ?>
-</body>
-</html>
+<form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
+    <div>
+        <label for="name">Name: </label>
+        <input type="text" name="name">
+    </div>
+    <div>
+        <label for="age">Age: </label>
+        <input type="number" name="age">
+    </div>
+    <input type="submit" name="submit" value="Submit">
+</form>
+
