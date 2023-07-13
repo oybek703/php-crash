3a53aa34d6072fdb23515b8fc9165f06a4b27d8f
Yoriqulov Oybek <hhoybek@gmail.com>
2023-07-13 20:48:05 +0500

    global get

--- index.php
+++ index.php
@@ -1,13 +1,32 @@
 <?php
-function sum($n1, $n2) {
-    return $n1+ $n2;
+
+function get_query_params(array $params_array): string
+{
+    $total = '';
+    $params_count = count($params_array);
+    if ($params_count == 0) return $total;
+    if ($params_count == 1) $total .= '?' . key($params_array) . $params_array[key($params_array)];
+    for ($i = 0; $i < $params_count; $i++) {
+        $current_key = array_keys($params_array)[$i];
+        $total .= ($i == 0 ? '?' : '&') . $current_key . '=' . $params_array[$current_key];
+    }
+    return $total;
 }
 
-$sum1 = sum(1,2);
-echo $sum1 . '</br>';
+$get_link = fn() => "<a href='{${get_query_params($_GET)}}'>Click</a>";
 
-$y=1;
+?>
 
-$f2 = fn ($n1, $n2) => $n1 + $n2 ;
-
-echo $f2(20, 87);
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?php echo $get_link() ?>
+</body>
+</html>
