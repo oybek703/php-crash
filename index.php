75dbc8ed32070b8f94dc913c62f74f86fef8444e
Yoriqulov Oybek <hhoybek@gmail.com>
2023-07-13 17:59:13 +0500

    loops

--- index.php
+++ index.php
@@ -1,30 +1,10 @@
 <?php
+$posts = ['First Post', 'Second Post', 'Third Post'];
 
-$time1 = date('F s');
-echo $time1;
+//for ($x = 0; $x < count($posts); $x++) {
+//    echo "$x - $posts[$x] </br>";
+//}
 
-if ($time1) {
-    echo "Time is {$time1}";
-} elseif ($time1 === 12) {
-    echo 'Time is 12';
-} else {
-    echo 'Time is not detected';
+foreach ($posts as $key => $value) {
+    echo "$key. $value </br>";
 }
-
-$color = 'blue';
-
-switch ($color) {
-    case 'blue':
-        echo 'Blue selected';
-        break;
-    case  'red':
-        echo 'Red selected';
-        break;
-    case  'green':
-        echo 'Green selected';
-        break;
-    default:
-        echo 'Red, Green, Blue is not selected';
-}
-
-?>
