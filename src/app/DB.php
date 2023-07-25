31bade0b60ff009afa80c0dee0fab38880c68023
Yoriqulov Oybek <hhoybek@gmail.com>
2023-07-25 12:48:44 +0500

    Singleton class example like db

---
+++ src/app/DB.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+class DB
+{
+    private static ?DB $instance = null;
+
+    private function __construct(array $config)
+    {
+        echo "Instance created! </br>";
+    }
+
+    public static function getInstance(array $config): DB
+    {
+        if (self::$instance === null) {
+            self::$instance = new DB($config);
+        }
+        return self::$instance;
+    }
+
+
+}
--- src/public/index.php
+++ src/public/index.php
@@ -3,9 +3,12 @@ declare(strict_types=1);
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+use App\DB;
 use App\PaymentGateway\Paddle\Transaction;
 
 $transaction = new Transaction(100, 'Test desc');
+$db = DB::getInstance([]);
 
+var_dump($db);
 echo $transaction::getCount();
 
