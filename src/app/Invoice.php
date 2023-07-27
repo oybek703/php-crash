4ae7e2239efab754805c1f30a00eee70b2783d1b
Yoriqulov Oybek <hhoybek@gmail.com>
2023-07-27 13:42:26 +0500

    __debugInfo

--- src/app/Invoice.php
+++ src/app/Invoice.php
@@ -7,15 +7,15 @@ namespace App;
 class
 Invoice
 {
+    private string $name = 'Test';
+    private int $accountId = 324567890765432;
 
-    public function __toString(): string
+    public function __debugInfo(): ?array
     {
-        echo ($this instanceof \Stringable);
-        return '1';
+        return [
+            'name' => $this->name,
+            'account_id' => '****   ' . substr((string)$this->accountId, -4),
+        ];
     }
 
-    public function __invoke()
-    {
-        echo "Invoked";
-    }
 }
