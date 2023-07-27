90e637c12b064604175232759b9be4a1d473b9fd
Yoriqulov Oybek <hhoybek@gmail.com>
2023-07-28 00:11:26 +0500

    set global exception handler

--- src/public/index.php
+++ src/public/index.php
@@ -8,17 +8,8 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 $inv1 = new Invoice(new Customer(['foo' => 'bar']));
 
-function process(): int | bool
-{
-    try {
-        global $inv1;
-        $inv1->process(-100);
-        return true;
-    } catch (Exception $e) {
-        echo $e->getMessage() . PHP_EOL;
-        return false;
-    }
-}
+set_exception_handler(function (Exception $e) {
+    var_dump($e->getMessage());
+});
 
-
-var_dump(process());
+array_rand([], 1);
