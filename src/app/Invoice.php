744b35372eed8fd0e2e7f41b14490573ac9f9dbe
Yoriqulov Oybek <hhoybek@gmail.com>
2023-07-27 17:14:24 +0500

    clone and __clone magic methods

--- src/app/Invoice.php
+++ src/app/Invoice.php
@@ -6,9 +6,16 @@ namespace App;
 
 class Invoice
 {
+    private static $id;
 
     public function __construct()
     {
+        $this->id = uniqid('invoice_');
+    }
+
+    public function __clone(): void
+    {
+        $this->id = uniqid('invoice_');
     }
 
     public static function create(): static
--- src/public/index.php
+++ src/public/index.php
@@ -5,9 +5,7 @@ declare(strict_types=1);
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $inv1 = new \App\Invoice();
-$inv2 = new \App\Invoice();
-
-$inv3 = \App\Invoice::create();
+$inv2 =  clone $inv1;
 
 
-var_dump($inv1, $inv2, $inv3);
+var_dump($inv1, $inv2, $inv1 === $inv2);
