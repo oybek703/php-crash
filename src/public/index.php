1d06959b41dd5fb7dbcbd118c7b7be3249681811
Yoriqulov Oybek <hhoybek@gmail.com>
2023-09-14 17:41:44 +0500

    switch

--- src/public/index.php
+++ src/public/index.php
@@ -1,6 +1,32 @@
 <?php
 
-$nums = [1, 2, 3, 4, 5];
-foreach($nums as $index => $num):
-    echo "nums[{$index}] = {$num} <br> ";
-endforeach;
+function temp() {
+    sleep(3);
+    echo "Done <br>";
+    return 2;
+}
+
+// Execution time takes 9 seconds overall
+//$t = temp();
+//
+//if ($t === 1) {
+//
+//} elseif ($t === 4 ) {
+//
+//} elseif ($t === 3) {
+//
+//}
+
+switch (temp()):
+    case 3:
+        echo temp();
+        break;
+    case 4:
+        echo temp();
+        break;
+    case 5:
+        echo temp();
+        break;
+    default:
+        echo null;
+endswitch;
