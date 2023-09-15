2f7ad8318d3b2700914b8a7a3501d600c452d57e
Yoriqulov Oybek <hhoybek@gmail.com>
2023-09-15 09:31:09 +0500

    static variables

--- src/public/index.php
+++ src/public/index.php
@@ -1,5 +1,19 @@
 <?php
 
-function test1(): ?int {
-    return null;
+function getValue() {
+    static $value = null;
+    if ($value == null) {
+        $value = someFunction();
+    }
+    return $value;
 }
+
+function someFunction() {
+    sleep(2);
+    echo "Processing...";
+    return 11;
+}
+
+echo getValue() . "<br>";
+echo getValue() . "<br>";
+echo getValue() . "<br>";
