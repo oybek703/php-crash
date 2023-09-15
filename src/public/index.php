a45c741ecbc1bd54d95c65d56ea1c4392d7c6a67
Yoriqulov Oybek <hhoybek@gmail.com>
2023-09-15 10:41:06 +0500

    some array functions

--- src/public/index.php
+++ src/public/index.php
@@ -1,11 +1,16 @@
 <?php
+declare(strict_types=1);
+$nums = [];
 
-$x =10;
+for ($i = 1; $i < 10; $i++) {
+    $nums["i_{$i}"] = $i;
+}
 
-$sum = function (int $y) use(&$x): int {
-    $x = 11;
-    return $x + $y;
-};
-echo $sum(10);
-echo "<br>";
-echo $x;
+
+$even = array_filter($nums, fn($n, $key) => $n % 2 === 0, ARRAY_FILTER_USE_BOTH);
+
+$r = array_map(fn($n) => $n * 100, $nums);
+
+echo "<pre>";
+print_r($r);
+echo "<pre>";
