09c0baf191c35951196be984e28d2ca3d01e0277
Yoriqulov Oybek <hhoybek@gmail.com>
2023-09-15 14:25:23 +0500

    error handling

--- src/public/index.php
+++ src/public/index.php
@@ -1,16 +1,9 @@
 <?php
-declare(strict_types=1);
-$nums = [];
+ini_set('display_errors', 1);
 
-for ($i = 1; $i < 10; $i++) {
-    $nums["i_{$i}"] = $i;
-}
+echo ini_get('max_execution_time');
+set_time_limit(2);
 
+sleep(3);
 
-$even = array_filter($nums, fn($n, $key) => $n % 2 === 0, ARRAY_FILTER_USE_BOTH);
-
-$r = array_map(fn($n) => $n * 100, $nums);
-
-echo "<pre>";
-print_r($r);
-echo "<pre>";
+echo "<br> Hi there";
