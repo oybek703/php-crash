b1a3bf18d60a1812089f456e1884d96f57a2b17d
Yoriqulov Oybek <hhoybek@gmail.com>
2023-09-19 11:46:57 +0500

    datetime objects

--- src/public/index.php
+++ src/public/index.php
@@ -3,20 +3,13 @@ declare(strict_types=1);
 ini_set('display_errors', '1');
 
 require_once '../vendor/autoload.php';
-use App\Invoice;
 
-set_exception_handler(function (Throwable $e) {
-    echo $e->getMessage() . ' ' . $e->getFile() .  ': ' . $e->getLine();
-});
+$from = new DateTime();
+$to = $from->add(new DateInterval('P1D'));
 
 
-$invoice1 = new Invoice(new \App\Customer());
+$date_format = 'm/d/y';
 
-echo array_rand([], 1);
-
-//try {
-//    $invoice1->process(2000) . PHP_EOL;
-//} catch (Throwable $exception) {
-//    echo $exception->getMessage() . PHP_EOL;
-//}
-
+echo "<pre>";
+    print_r($from->format($date_format) . ' - '  . $to->format($date_format));
+echo "</pre>";
