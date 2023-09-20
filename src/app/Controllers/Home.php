4ace0ab576edff754e5c911be75771d6413b3fc8
Yoriqulov Oybek <hhoybek@gmail.com>
2023-09-20 11:44:55 +0500

    pdo statement with bind

--- src/app/Controllers/Home.php
+++ src/app/Controllers/Home.php
@@ -5,37 +5,39 @@ declare(strict_types=1);
 namespace App\Controllers;
 
 use App\View;
-use Cassandra\Date;
+use PDO;
+use PDOException;
 
 class Home
 {
     public function index(): View
     {
         try {
-            $db = new \PDO(
+            $db = new PDO(
                 'mysql:host=db;dbname=php_crash_db',
                 'root',
                 'root'
             );
-            $email = 'sara1@gmail.com';
+            $email = 'sara3@gmail.com';
             $fullName = 'Sara Fly';
             $isActive = 1;
             $createdAt = date('Y-m-d H:i:s', (int)strtotime('08.07.2021 12:05PM'));
             $query = "insert into users(email, full_name, is_active, created_at)
                       values (:email, :fullName, :isActive, :createdAt)";
             $statement = $db->prepare($query);
-            $statement->execute([
-                'email' => $email,
-                'fullName' => $fullName,
-                'isActive' => $isActive,
-                'createdAt' => $createdAt
-            ]);
+
+            $statement->bindValue('email', $email);
+            $statement->bindValue('fullName', $fullName);
+            $statement->bindParam('isActive', $isActive, PDO::PARAM_BOOL);
+            $statement->bindValue('createdAt', $createdAt);
+            $statement->execute();
+
             $id = (int) $db->lastInsertId();
             $user = $db->query('SELECT * FROM users WHERE id=' . $id)->fetchAll();
             echo "<pre>";
             print_r($user);
             echo "</pre>";
-        } catch (\PDOException $exception) {
+        } catch (PDOException $exception) {
             throw new \PDOException($exception->getMessage(), (int)$exception->getCode());
         }
 
